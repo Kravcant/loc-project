5863bea4d62ff9e9bb5e944ff38193da33489594
anthonykravchishin <tonykravc53@gmail.com>
2025-10-31 12:11:36 -0700

    Addded node integration

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3100;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended:true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
